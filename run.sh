953b10c8af304e8c839a26c749ba6210bf799dfe
Martin Lacko <martin.lacko@simplestaking.com>
2020-04-06 10:13:56 +0200

    Changed run script to work off release mode rather than debug mode

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@ set -e
 
 trap ./clean.sh INT
 
-cargo build
-sudo setcap cap_net_raw,cap_net_admin=eip ./target/debug/tezedge_proxy
-sudo RUST_BACKTRACE=1 ./target/debug/tezedge_proxy \
+cargo build --release
+sudo setcap cap_net_raw,cap_net_admin=eip ./target/release/tezedge_proxy
+sudo RUST_BACKTRACE=1 ./target/release/tezedge_proxy \
   --identity-file ./identity/identity.json \
-  --port 9732 --interface enp34s0 \
-  --local-address 192.168.1.199
+  --port 9732 --interface eth0 \
+  --local-address 192.168.70.132
