088a4cf111c3dbba439c739900416eb3088e49c9
Martin Lacko <martin.lacko@simplestaking.com>
2020-04-09 17:42:31 +0200

    Fixed bad argument in `run.sh`

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@ cargo build --release
 sudo setcap cap_net_raw,cap_net_admin=eip ./target/release/tezedge_proxy
 sudo ./target/release/tezedge_proxy \
   --identity-file ./identity/identity.json \
-  --port 9732 --interface eth0 \
+  --rpc-port 9732 --interface eth0 \
   --local-address 192.168.1.199
 
 bash ./clean.sh
