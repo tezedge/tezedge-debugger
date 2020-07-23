2cc933b7be6bdcdcf039283f042938ab4d387c1f
Vladislav Melnik <vladislav.melnik@simplestaking.com>
2020-07-23 21:00:26 +0300

    add disk io and cpu metrics

--- src/messages/metric_message.rs
+++ src/messages/metric_message.rs
@@ -1,6 +1,7 @@
 use serde::{Serialize, Deserialize};
 use storage::persistent::{Decoder, Encoder, SchemaError};
 use chrono::{DateTime, Utc, TimeZone};
+use std::collections::BTreeMap;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct MetricMessage(pub ContainerStats);
@@ -44,22 +45,141 @@ impl ContainerSpec {
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ContainerStats {
-    #[serde(with = "date_format")]
+    //#[serde(with = "date_format")]
     pub timestamp: DateTime<Utc>,
+
+    #[serde(default)]
+    pub cpu: CpuStats,
+
+    #[serde(default)]
+    pub diskio: DiskIoStats,
 
     #[serde(default)]
     pub memory: MemoryStats,
 }
 
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
+// All CPU usage metrics are cumulative from the creation of the container
+pub struct CpuStats {
+    pub usage: CpuUsage,
+    pub cfs: CpuCFS,
+    pub schedstat: Schedstat,
+	// Smoothed average of number of runnable threads x 1000.
+	// We multiply by thousand to avoid using floats, but preserving precision.
+	// Load is smoothed over the last 10 seconds. Instantaneous value can be read
+	// from LoadStats.NrRunning.
+    pub load_average: i32,
+}
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+// CPU usage time statistics.
+pub struct CpuUsage {
+	// Total CPU usage.
+	// Unit: nanoseconds.
+    pub total: u64,
+
+    // Per CPU/core usage of the container.
+	// Unit: nanoseconds.
+    pub per_cpu_usage: Vec<u64>,
+
+	// Time spent in user space.
+	// Unit: nanoseconds.
+    pub user: u64,
+
+	// Time spent in kernel space.
+	// Unit: nanoseconds.
+    pub system: u64,
+}
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+// Cpu Completely Fair Scheduler statistics.
+pub struct CpuCFS {
+	// Total number of elapsed enforcement intervals.
+    pub periods: u64,
+
+	// Total number of times tasks in the cgroup have been throttled.
+    pub throttled_periods: u64,
+
+	// Total time duration for which tasks in the cgroup have been throttled.
+	// Unit: nanoseconds.
+    pub throttled_time: u64,
+}
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+// Cpu Aggregated scheduler statistics
+pub struct Schedstat {
+	// https://www.kernel.org/doc/Documentation/scheduler/sched-stats.txt
+
+	// time spent on the cpu
+    pub run_time: u64,
+
+    // time spent waiting on a runqueue
+    pub runqueue_time: u64,
+
+	// # of timeslices run on this cpu
+    pub run_periods: u64,
+}
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+pub struct DiskIoStats {
+    #[serde(default)]
+    pub io_service_bytes: Vec<PerDiskStats>,
+    #[serde(default)]
+    pub io_serviced: Vec<PerDiskStats>,
+    #[serde(default)]
+    pub io_queued: Vec<PerDiskStats>,
+    #[serde(default)]
+    pub sectors: Vec<PerDiskStats>,
+    #[serde(default)]
+    pub io_service_time: Vec<PerDiskStats>,
+    #[serde(default)]
+    pub io_wait_time: Vec<PerDiskStats>,
+    #[serde(default)]
+    pub io_merged: Vec<PerDiskStats>,
+    #[serde(default)]
+    pub io_time: Vec<PerDiskStats>,
+}
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+pub struct PerDiskStats {
+    pub device: String,
+    pub major: u64,
+    pub minor: u64,
+    pub stats: BTreeMap<String, u64>,
+}
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct MemoryStats {
+	// Current memory usage, this includes all memory regardless of when it was
+	// accessed.
+	// Units: Bytes.
     pub usage: u64,
+
+	// Maximum memory usage recorded.
+	// Units: Bytes.
     pub max_usage: u64,
+
+	// Number of bytes of page cache memory.
+	// Units: Bytes.
     pub cache: u64,
+
+	// The amount of anonymous and swap cache memory (includes transparent
+	// hugepages).
+	// Units: Bytes.
     pub rss: u64,
+
+	// The amount of swap currently used by the processes in this cgroup
+	// Units: Bytes.
     pub swap: u64,
+
+	// The amount of memory used for mapped files (includes tmpfs/shmem)
     pub mapped_file: u64,
+
+	// The amount of working set memory, this includes recently accessed memory,
+	// dirty memory, and kernel memory. Working set is <= "usage".
+	// Units: Bytes.
     pub working_set: u64,
+
     pub failcnt: u64,
 
     #[serde(default)]
@@ -110,77 +230,6 @@ impl Encoder for MetricMessage {
     }
 }
 
-impl Decoder for ContainerStats {
-    fn decode(bytes: &[u8]) -> Result<Self, SchemaError> {
-        serde_cbor::from_slice(bytes)
-            .map_err(|_| SchemaError::DecodeError)
-    }
-}
-
-impl Encoder for ContainerStats {
-    fn encode(&self) -> Result<Vec<u8>, SchemaError> {
-        serde_cbor::to_vec(self)
-            .map_err(|_| SchemaError::EncodeError)
-    }
-}
-
-impl Decoder for MemoryStats {
-    fn decode(bytes: &[u8]) -> Result<Self, SchemaError> {
-        serde_cbor::from_slice(bytes)
-            .map_err(|_| SchemaError::DecodeError)
-    }
-}
-
-impl Encoder for MemoryStats {
-    fn encode(&self) -> Result<Vec<u8>, SchemaError> {
-        serde_cbor::to_vec(self)
-            .map_err(|_| SchemaError::EncodeError)
-    }
-}
-
-impl Decoder for MemoryStatsMemoryData {
-    fn decode(bytes: &[u8]) -> Result<Self, SchemaError> {
-        serde_cbor::from_slice(bytes)
-            .map_err(|_| SchemaError::DecodeError)
-    }
-}
-
-impl Encoder for MemoryStatsMemoryData {
-    fn encode(&self) -> Result<Vec<u8>, SchemaError> {
-        serde_cbor::to_vec(self)
-            .map_err(|_| SchemaError::EncodeError)
-    }
-}
-
-// copied from https://serde.rs/custom-date-format.html
-mod date_format {
-    use chrono::{DateTime, Utc, TimeZone};
-    use serde::{self, Deserialize, Serializer, Deserializer};
-
-    const FORMAT: &'static str = "%Y-%m-%dT%H:%M:%S.%fZ";
-
-    pub fn serialize<S>(
-        date: &DateTime<Utc>,
-        serializer: S,
-    ) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let s = format!("{}", date.format(FORMAT));
-        serializer.serialize_str(&s)
-    }
-
-    pub fn deserialize<'de, D>(
-        deserializer: D,
-    ) -> Result<DateTime<Utc>, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let s = String::deserialize(deserializer)?;
-        Utc.datetime_from_str(&s, FORMAT).map_err(serde::de::Error::custom)
-    }
-}
-
 #[cfg(test)]
 #[test]
 fn deserialize_container_info_map_from_json() {
@@ -188,6 +237,5 @@ fn deserialize_container_info_map_from_json() {
 
     // curl 'http://localhost:8080/api/v1.3/docker' > tests/metrics_data.json
     const DATA: &str = include_str!("../../tests/metrics_data.json");
-    let info = serde_json::from_str::<HashMap<String, ContainerInfo>>(DATA).unwrap();
-    println!("{:#?}", info);
+    assert!(serde_json::from_str::<HashMap<String, ContainerInfo>>(DATA).is_ok());
 }
