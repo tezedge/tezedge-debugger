b1c42a11470d239f81fb380178c1391a14aea092
Vladislav Melnik <vladislav.melnik@simplestaking.com>
2020-10-06 22:54:17 +0300

    remove dead code

--- src/utility/mod.rs
+++ src/utility/mod.rs
@@ -2,12 +2,10 @@
 // SPDX-License-Identifier: MIT
 
 pub mod stream;
-pub mod decrypter;
 pub mod identity;
 pub mod ip_settings;
 
 pub mod prelude {
-    pub use super::decrypter::{P2pDecrypter, DecryptedChunk};
     pub use super::identity::Identity;
-    pub use super::ip_settings::*;
+    pub use super::ip_settings::get_local_ip;
 }
