6e489afaed0d3d7edad7793c0584ba932e0d914b
Vladislav Melnik <vladislav.melnik@simplestaking.com>
2020-12-19 14:33:06 +0000

    use typenum

--- sniffer/src/bin/kprobe.rs
+++ sniffer/src/bin/kprobe.rs
@@ -4,7 +4,8 @@
 
 use redbpf_probes::kprobe::prelude::*;
 use redbpf_probes::helpers::gen;
-use core::{mem, ptr, result::Result, slice};
+use typenum::Unsigned;
+use core::{mem, ptr, result::Result, slice, ops::Sub};
 use sniffer::DataDescriptor;
 
 program!(0xFFFFFFFE, "GPL");
@@ -18,70 +19,64 @@ static mut main_buffer: RingBuffer = RingBuffer::with_max_length(0x80000000);
 const DD: usize = mem::size_of::<DataDescriptor>();
 
 #[inline(always)]
-fn send_piece(
+fn send_piece<S, C>(
     tag: i32,
-    fd: u32,
     data: &[u8],
-    size: usize,
-    offset: usize,
-    overall_size: usize,
-) -> Result<(), ()> {
-    match unsafe { main_buffer.reserve(size as u64, 0) } {
+    header_ctor: C,
+) -> Result<(), ()>
+where
+    // Sub<typenum::U20>, 20 should be equal size_of DataDescriptor
+    S: Unsigned + Sub<typenum::U20>,
+    <S as Sub<typenum::U20>>::Output: Unsigned,
+    C: FnOnce(i32) -> DataDescriptor,
+{
+    match unsafe { main_buffer.reserve(S::U64, 0) } {
         Ok(buffer) => {
             let result = unsafe {
                 let source = data.as_ptr();
                 let destination = buffer.0.as_mut_ptr().offset(DD as isize);
                 gen::bpf_probe_read_user(
                     destination as *mut _,
-                    (size - DD) as u32,
+                    <<S as Sub<typenum::U20>>::Output as Unsigned>::U32,
                     source as *const _,
                 )
             };
-            let descriptor = DataDescriptor {
-                tag: if result < 0 { result as i32 } else { tag },
-                fd: fd,
-                offset: offset as u32,
-                size: size as u32,
-                overall_size: overall_size as u32,
-            };
+            let descriptor = header_ctor(if result < 0 { result as i32 } else { tag });
             unsafe {
-                ptr::write_unaligned(buffer.0[..DD].as_ptr() as *mut _, descriptor);
+                ptr::write(buffer.0[..DD].as_ptr() as *mut _, descriptor);
             }
             buffer.submit(0);
             if result < 0 {
-                return Err(());
+                Err(())
+            } else {
+                Ok(())
             }
-            Ok(())
         },
         Err(()) => {
             // failed to allocate buffer, try allocate smaller buffer to report error
-            match unsafe { main_buffer.reserve(DD as u64, 0) } {
-                Ok(buffer) => {
-                    let descriptor = DataDescriptor {
-                        tag: -90,
-                        fd: fd,
-                        offset: offset as u32,
-                        size: size as u32,
-                        overall_size: overall_size as u32,
-                    };
-                    unsafe {
-                        ptr::write_unaligned(buffer.0.as_ptr() as *mut _, descriptor);
-                    }
-                    buffer.submit(0);
-                },
-                Err(()) => {
-                    // failed to allocate buffer to report error, nothing to do
-                },
+            let buffer = unsafe { main_buffer.reserve(DD as u64, 0) }?;
+            let descriptor = header_ctor(-90);
+            unsafe {
+                ptr::write(buffer.0.as_ptr() as *mut _, descriptor);
             }
+            buffer.submit(0);
             Err(())
         },
     }
 }
 
 macro_rules! send_piece {
-    ($size:expr, $tag:expr, $fd:expr, $data:expr, $offset:expr) => {{
-        send_piece($tag, $fd, &$data[$offset..], $size, $offset, $data.len())?;
-        $offset += $size - DD;
+    ($size:ty, $tag:expr, $fd:expr, $data:expr, $offset:expr) => {{
+        send_piece::<$size, _>($tag, &$data[$offset..], |tag| {
+            DataDescriptor {
+                tag: tag,
+                fd: $fd,
+                offset: $offset as u32,
+                size: <$size>::U32,
+                overall_size: $data.len() as u32,
+            }
+        })?;
+        $offset += <$size as Unsigned>::USIZE - DD;
         if $offset >= $data.len() {
             Err(())?
         }
@@ -92,29 +87,29 @@ macro_rules! send_piece {
 fn send_data(tag: i32, fd: u32, data: &[u8]) -> Result<(), ()> {
     let mut offset = 0;
 
-    send_piece!(0x0000_0100, tag, fd, data, offset);
-    send_piece!(0x0000_0100, tag, fd, data, offset);
-    send_piece!(0x0000_0200, tag, fd, data, offset);
-    send_piece!(0x0000_0400, tag, fd, data, offset);
-    send_piece!(0x0000_0800, tag, fd, data, offset);
-    send_piece!(0x0000_1000, tag, fd, data, offset);
-    send_piece!(0x0000_2000, tag, fd, data, offset);
-    send_piece!(0x0000_4000, tag, fd, data, offset);
-    send_piece!(0x0000_8000, tag, fd, data, offset);
-    send_piece!(0x0001_0000, tag, fd, data, offset);
-    send_piece!(0x0002_0000, tag, fd, data, offset);
-    send_piece!(0x0004_0000, tag, fd, data, offset);
-    send_piece!(0x0008_0000, tag, fd, data, offset);
-    send_piece!(0x0010_0000, tag, fd, data, offset);
-    send_piece!(0x0020_0000, tag, fd, data, offset);
-    send_piece!(0x0040_0000, tag, fd, data, offset);
-    send_piece!(0x0080_0000, tag, fd, data, offset);
-    send_piece!(0x0100_0000, tag, fd, data, offset);
-    send_piece!(0x0200_0000, tag, fd, data, offset);
-    send_piece!(0x0400_0000, tag, fd, data, offset);
-    send_piece!(0x0800_0000, tag, fd, data, offset);
-    send_piece!(0x1000_0000, tag, fd, data, offset);
-    send_piece!(0x2000_0000, tag, fd, data, offset);
+    send_piece!(typenum::U256, tag, fd, data, offset);
+    send_piece!(typenum::U256, tag, fd, data, offset);
+    send_piece!(typenum::U512, tag, fd, data, offset);
+    send_piece!(typenum::U1024, tag, fd, data, offset);
+    send_piece!(typenum::U2048, tag, fd, data, offset);
+    send_piece!(typenum::U4096, tag, fd, data, offset);
+    send_piece!(typenum::U8192, tag, fd, data, offset);
+    send_piece!(typenum::U16384, tag, fd, data, offset);
+    send_piece!(typenum::U32768, tag, fd, data, offset);
+    send_piece!(typenum::U65536, tag, fd, data, offset);
+    send_piece!(typenum::U131072, tag, fd, data, offset);
+    send_piece!(typenum::U262144, tag, fd, data, offset);
+    send_piece!(typenum::U524288, tag, fd, data, offset);
+    send_piece!(typenum::U1048576, tag, fd, data, offset);
+    send_piece!(typenum::U2097152, tag, fd, data, offset);
+    send_piece!(typenum::U4194304, tag, fd, data, offset);
+    send_piece!(typenum::U8388608, tag, fd, data, offset);
+    send_piece!(typenum::U16777216, tag, fd, data, offset);
+    send_piece!(typenum::U33554432, tag, fd, data, offset);
+    send_piece!(typenum::U67108864, tag, fd, data, offset);
+    send_piece!(typenum::U134217728, tag, fd, data, offset);
+    send_piece!(typenum::U268435456, tag, fd, data, offset);
+    send_piece!(typenum::U536870912, tag, fd, data, offset);
 
     Ok(())
 }
