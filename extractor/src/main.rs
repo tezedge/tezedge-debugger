6ce3c135cd00e432fec8bb2a765fb45575ee487f
Alexander Koptelov <alexander.koptelov@viablesystems.io>
2021-04-15 22:05:43 +0300

    Extract data suitable for fuzzing

--- extractor/src/main.rs
+++ extractor/src/main.rs
@@ -1,10 +1,55 @@
+use std::{collections::HashMap, path::PathBuf};
+
+use tokio::fs::{File, create_dir};
+use tokio::io::AsyncWriteExt;
+
 #[tokio::main]
 async fn main() {
-    let types = "connection_message,metadata,ack_message,disconnect,advertise,swap_request,\
-        swap_ack,bootstrap,get_current_branch,current_branch,deactivate,\
-        get_current_head,current_head,get_block_headers,block_header,get_operations,operation\
-        get_protocols,protocol,get_operation_hashes_for_blocks,operation_hashes_for_block,\
-        get_operations_for_blocks,operations_for_blocks";
+    let mapping = [
+        ("p2p", "PeerMessageResponse"),
+        ("connection", "ConnectionMessage"),
+        ("meta", "Metadata"),
+    ]
+    .iter()
+    .cloned()
+    .collect::<HashMap<_, _>>();
+
+    #[derive(serde::Deserialize, Debug)]
+    struct Message {
+        category: String,
+        kind: Option<String>,
+        id: u64,
+        incoming: bool,
+    }
+
+    impl Message {
+        fn get_file_name(&self) -> String {
+            if self.category == "p2p" {
+                format!(
+                    "{}.{}.msg",
+                    self.id,
+                    self.kind.as_ref().expect("kind is expected")
+                )
+            } else {
+                format!("{}.msg", self.id)
+            }
+        }
+    }
+
+    #[derive(serde::Deserialize, Debug)]
+    struct MessageBytes {
+        decrypted_bytes: Vec<String>,
+    }
+
+    impl From<MessageBytes> for Vec<u8> {
+        fn from(source: MessageBytes) -> Vec<u8> {
+            source
+                .decrypted_bytes
+                .into_iter()
+                .map(|s| hex::decode(s).expect("can't decode")[0])
+                .collect()
+        }
+    }
 
     #[derive(serde::Serialize)]
     struct Example<'a> {
@@ -12,40 +57,67 @@ async fn main() {
         hex: String,
     }
 
-    let mut examples = Vec::<Example>::new();
-    for ty in types.split(',') {
-        let url = format!("http://debug.dev.tezedge.com:17742/v3/messages?types={}&limit=1", ty);
-        let list = reqwest::get(url)
-            .await.unwrap()
-            .text()
-            .await.unwrap();
-        let list = serde_json::from_str::<serde_json::Value>(&list).unwrap();
-
-        if let Some(item) = list.as_array().and_then(|x| x.first()) {
-            let id = item.as_object().unwrap().get("id").unwrap().as_u64().unwrap();
-            let url = format!("http://debug.dev.tezedge.com:17742/v3/message/{}", id);
-            let item = reqwest::get(url)
-                .await.unwrap()
-                .text()
-                .await.unwrap();
-            let item = serde_json::from_str::<serde_json::Value>(&item).unwrap();
-            let o = item.as_object().unwrap().get("decrypted_bytes").unwrap().as_array().unwrap();
-            let data = o.iter()
-                .map(|i| {
-                    let s = i.as_str().unwrap();
-                    hex::decode(s).unwrap().into_iter()
-                })
-                .flatten()
-                .collect::<Vec<_>>();
-            examples.push(Example {
-                ty,
-                hex: hex::encode(&data),
-            });
+    let mut cursor = 0;
+    loop {
+        let url = if cursor == 0 {
+            format!(
+                "http://debug.dev.tezedge.com:17742/v3/messages?limit={}",
+                100
+            )
         } else {
-            eprintln!("warning: no example for type: {}", ty);
+            format!(
+                "http://debug.dev.tezedge.com:17742/v3/messages?limit={}&cursor={}",
+                100, cursor
+            )
+        };
+        let list = reqwest::get(url)
+            .await
+            .unwrap()
+            .json::<Vec<Message>>()
+            .await
+            .unwrap();
+
+        let mut message_id = 0;
+        for message in list {
+            if cursor > 0 && message.id > cursor {
+                continue
+            }
+
+            let url = format!(
+                "http://debug.dev.tezedge.com:17742/v3/message/{}",
+                message.id
+            );
+            let decrypted = reqwest::get(url)
+                .await
+                .unwrap()
+                .json::<MessageBytes>()
+                .await
+                .unwrap();
+
+            let decrypted: Vec<u8> = decrypted.into();
+            let path = PathBuf::from(mapping[message.category.as_str()]);
+            if !path.exists() {
+                create_dir(&path).await.expect("cannot create dir");
+            } else {
+                assert!(path.is_dir());
+            }
+
+            let path = path.join(message.get_file_name());
+
+            println!("-> {}", path.to_string_lossy());
+            File::create(path)
+                .await
+                .expect("cannot create file")
+                .write_all(&decrypted)
+                .await
+                .expect("cannot write data");
+
+            message_id = message.id;
+        }
+        if message_id == 0 {
+            break;
+        } else {
+            cursor = message_id - 1;
         }
     }
-
-    let json = serde_json::to_string(&examples).unwrap();
-    println!("{}", json);
 }
