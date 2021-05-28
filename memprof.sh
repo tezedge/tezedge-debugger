73a4e9bd6e6ddfc5e2c638323b264b9c9c2708be
Vladislav Melnik <vladislav.melnik@simplestaking.com>
2021-05-28 09:20:54 +0000

    improve script

--- memprof.sh
+++ memprof.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/bash
 
-docker inspect --format='{{index .RepoDigests 0}}' ${TEZEDGE_IMAGE} > hash.new
-
-if [[ $(< hash) == $(< hash.new) ]]; then
-    bpf-memprof-user & sleep 0.5
-    while ! docker cp $(docker ps -qf ancestor=${TEZEDGE_IMAGE}):/light-node /; do
-        sleep 0.5
-    done
-    tezedge-memprof
-else
-    echo "image hash mismatch\\nexpected: $(< hash)\\nhave: $(< hash.new)"
-    exit 123
-fi
+# TODO: rewrite in rust
+bpf-memprof-user & sleep 0.5
+while ! docker cp $(docker ps -qf ancestor=$(< hash)):/light-node /; do
+    sleep 0.5
+done
+tezedge-memprof
