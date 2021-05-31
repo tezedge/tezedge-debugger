ac32a0af046b89f9169637f8b7cf81a17899faa5
Vladislav Melnik <vladislav.melnik@simplestaking.com>
2021-05-31 06:30:37 +0000

    remove image hash check, it was wrong approach

--- memprof.sh
+++ memprof.sh
@@ -2,7 +2,7 @@
 
 # TODO: rewrite in rust
 bpf-memprof-user & sleep 0.5
-while ! docker cp $(docker ps -qf ancestor=$(< hash)):/light-node /; do
+while ! docker cp $(docker ps -qf ancestor=$TEZEDGE_IMAGE):/light-node /; do
     sleep 0.5
 done
 tezedge-memprof
