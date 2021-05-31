915f6be9406e5d3017de174c1d9e27aede4dfd91
Vladislav Melnik <vladislav.melnik@simplestaking.com>
2021-05-31 09:11:34 +0000

    try drone

--- tests/wait_until.sh
+++ tests/wait_until.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-URL="localhost:18732/chains/main/blocks/head"
+
 HTTPD="0"
 until [ "$HTTPD" == "200" ]; do
   HTTPD=$(curl -A "Web Check" -sL --connect-timeout 3 -w "%{http_code}\n" "$URL" -o /dev/null)
