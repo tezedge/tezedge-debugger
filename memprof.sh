1818ffa2d90e1d6a86f4fd2d92b9598bf123ed3c
Vladislav Melnik <vladislav.melnik@simplestaking.com>
2021-05-31 10:38:51 +0000

    setup drone

--- memprof.sh
+++ memprof.sh
@@ -2,7 +2,7 @@
 
 # TODO: rewrite in rust
 bpf-memprof-user & sleep 0.5
-while ! docker cp $(docker ps -qf ancestor=$TEZEDGE_IMAGE):/light-node /; do
+while ! docker cp $(docker ps -qf name=$TEZEDGE_NODE_NAME):/light-node /; do
     sleep 0.5
 done
 tezedge-memprof
