9d29f83babf0c254e2d1ce167ba5e7425529d9cb
Vladislav Melnik <vladislav.melnik@simplestaking.com>
2021-07-23 17:00:52 +0000

    fix lost indexes

--- tezedge-recorder/src/database/search.rs
+++ tezedge-recorder/src/database/search.rs
@@ -1,10 +1,4 @@
-use std::{
-    fs,
-    ops::DerefMut,
-    path::Path,
-    sync::{Arc, Mutex, TryLockError, atomic::{Ordering, AtomicBool}},
-    thread,
-};
+use std::{fs, ops::DerefMut, path::Path, sync::{Arc, Mutex, MutexGuard, TryLockError, atomic::{Ordering, AtomicBool}}, thread};
 use tantivy::{
     directory::MmapDirectory,
     schema,
@@ -19,20 +13,47 @@ use tantivy::{
 
 pub struct LogIndexer {
     index: Index,
-    id_field: schema::Field,
-    message_field: schema::Field,
     writer_thread: Option<thread::JoinHandle<()>>,
-    queue: Arc<Mutex<(u64, Vec<String>)>>,
-    commit_state: Arc<State>,
+    commit_state: Arc<CommitState>,
 }
 
-struct State {
+#[derive(Default)]
+struct DocumentQueue {
+    start_id: u64,
+    messages: Vec<String>,
+}
+
+impl DocumentQueue {
+    fn enqueue(&mut self, id: u64, msg: &str) {
+        if self.messages.is_empty() {
+            self.start_id = id;
+        }
+        self.messages.push(msg.to_string());
+    }
+
+    fn is_empty(&self) -> bool {
+        self.messages.is_empty()
+    }
+
+    fn drain(self) -> impl Iterator<Item = (u64, String)> {
+        let id = self.start_id;
+        self.messages
+            .into_iter()
+            .enumerate()
+            .map(move |(i, msg)| (id + (i as u64), msg))
+    }
+}
+
+struct CommitState {
+    id_field: schema::Field,
+    message_field: schema::Field,
+    queue: Arc<Mutex<DocumentQueue>>,
     dirty: AtomicBool,
     running: AtomicBool,
     writer: Mutex<IndexWriter>,
 }
 
-impl State {
+impl CommitState {
     fn run(&self) {
         use std::time::{Instant, Duration};
 
@@ -44,18 +65,45 @@ impl State {
             Duration::from_micros(tick - (duration % tick))
         }
 
-        while self.running.load(Ordering::Relaxed) {
+        loop {
             let commit_begin = Instant::now();
             if self.dirty.fetch_and(false, Ordering::SeqCst) {
-                match self.writer.lock().unwrap().commit() {
+                let mut writer = self.finalize();
+                match writer.commit() {
                     Ok(_) => (),
                     Err(error) => log::error!("cannot commit log index: {:?}", error),
                 }
+            }
+            if !self.running.load(Ordering::Relaxed) {
+                break;
             }
             let commit_end = Instant::now();
             let elapsed = commit_end.duration_since(commit_begin);
             thread::sleep(align(elapsed, TICK));
         }
+    }
+
+    fn finalize(&self) -> MutexGuard<IndexWriter> {
+        use std::mem;
+
+        let writer = self.writer.lock().unwrap();
+        let mut queue_lock = self.queue.lock().unwrap();
+        if !queue_lock.is_empty() {
+            let queue = mem::replace(queue_lock.deref_mut(), DocumentQueue::default());
+            drop(queue_lock);
+            for (id, message) in queue.drain() {
+                writer.add_document(self.prepare_doc(&message, id));
+            }
+            self.dirty.fetch_or(true, Ordering::SeqCst);
+        }
+        writer
+    }
+
+    fn prepare_doc(&self, message: &str, id: u64) -> Document {
+        let mut doc = Document::default();
+        doc.add_text(self.message_field, message);
+        doc.add_u64(self.id_field, id);
+        doc
     }
 }
 
@@ -88,10 +136,13 @@ impl LogIndexer {
         let message_field = schema.get_field("message").unwrap();
         let id_field = schema.get_field("id").unwrap();
         let queue = Default::default();
-        let commit_state = Arc::new(State {
+        let commit_state = Arc::new(CommitState {
             dirty: AtomicBool::new(false),
             running: AtomicBool::new(true),
             writer: Mutex::new(index.writer(Self::HEAP_BYTES)?),
+            id_field,
+            message_field,
+            queue,
         });
 
         let writer_thread = {
@@ -99,7 +150,7 @@ impl LogIndexer {
             Some(thread::spawn(move || commit_state.run()))
         };
 
-        Ok(LogIndexer { index, message_field, id_field, queue, writer_thread, commit_state })
+        Ok(LogIndexer { index, writer_thread, commit_state })
     }
 
     pub fn write(&self, message: &str, id: u64) {
@@ -107,24 +158,17 @@ impl LogIndexer {
 
         match self.commit_state.writer.try_lock() {
             Ok(writer) => {
-                let (base, mut queue) = mem::replace(self.queue.lock().unwrap().deref_mut(), (0, Vec::new()));
-                queue.push(message.to_string());
-                for (i, message) in queue.into_iter().enumerate() {
-                    let mut doc = Document::default();
-                    doc.add_text(self.message_field, message);
-                    doc.add_u64(self.id_field, base + (i as u64));
-                    writer.add_document(doc);
+                let mut queue_lock = self.commit_state.queue.lock().unwrap();
+                let queue = mem::replace(queue_lock.deref_mut(), DocumentQueue::default());
+                drop(queue_lock);
+                for (id, message) in queue.drain() {
+                    writer.add_document(self.commit_state.prepare_doc(&message, id));
                 }
+                writer.add_document(self.commit_state.prepare_doc(message, id));
                 self.commit_state.dirty.fetch_or(true, Ordering::SeqCst);
             },
             Err(TryLockError::Poisoned(e)) => Err::<(), _>(e).unwrap(),
-            Err(TryLockError::WouldBlock) => {
-                let mut queue = self.queue.lock().unwrap();
-                if queue.1.is_empty() {
-                    queue.0 = id;
-                }
-                queue.1.push(message.to_string());
-            }
+            Err(TryLockError::WouldBlock) => self.commit_state.queue.lock().unwrap().enqueue(id, message),
         }
     }
 
@@ -138,9 +182,9 @@ impl LogIndexer {
             .reload_policy(ReloadPolicy::OnCommit)
             .try_into()?;
         let searcher = reader.searcher();
-        let query_parser = QueryParser::for_index(&self.index, vec![self.message_field]);
+        let query_parser = QueryParser::for_index(&self.index, vec![self.commit_state.message_field]);
         let query = query_parser.parse_query(query)?;
-        let id_field = self.id_field;
+        let id_field = self.commit_state.id_field;
         let it = searcher
             .search(&query, &TopDocs::with_limit(limit))?
             .into_iter()
